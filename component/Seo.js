8c9e98a778184ff3a8ed8ac14f21abdf7ec325fd
jalal1985khan <jalal1985khan@gmail.com>
2024-01-30 12:12:48 +0530

    header updated

--- component/Seo.js
+++ component/Seo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-
+import Script from 'next/script';
 function NextSeo({ title, description, path, metaImage }) {
     return (
         <>
@@ -32,7 +32,8 @@ function NextSeo({ title, description, path, metaImage }) {
                         "query-input": "required name=search_term_string"
                     }
                 })}
-            </script>
+                </script>
+                <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></Script>
             </Head>
 
            
--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import Script from "next/script";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
+      <body >{children}
       <link rel="icon" href="/images/cac_favicon-150x150.png" />
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></Script>
        {/* Start Google Analytics Manager */}
@@ -52,10 +52,6 @@ export default function RootLayout({ children }) {
         ></iframe>
       </noscript>
         {/* End Google Tag Manager (noscript) */}
-        </Head>
-      <body >{children}
-      
-      
       
       </body>
     </html>
